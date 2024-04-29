b4c1f41138f01ae28e6ff925916d1e35d9ec8991
damirusso95 <damirusso95@gmail.com>
2024-04-29 14:45:14 +0200

    first commit

---
+++ palindromi/pari o dispari/main.js
@@ -0,0 +1,5 @@
+// esercizio palindromi consrgna:
+// Sono accettate solo soluzioni che fanno uso del ciclo.
+// 1)Chiedere all’utente di inserire una parola.
+// 2)Creare una funzione per capire se la parola inserita è palindroma.
+//   Bonus: soluzioni più "comode"
