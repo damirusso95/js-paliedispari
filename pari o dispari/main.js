ee2227e68bad2d1f158b3542277e9afa22912301
damirusso95 <damirusso95@gmail.com>
2024-04-29 15:09:31 +0200

    pari dispari first commit

---
+++ pari o dispari/main.js
@@ -0,0 +1,7 @@
+// pari e dispari consegna:
+// 1) L’utente sceglie su cosa puntare: pari o dispari.
+// 2) L'utente inserisce un numero da 1 a 5.
+// 3) Il computer estrae un numero casuale sempre tra 1 e 5 (usando una funzione).
+// 4) Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+// 5) Dichiariamo chi ha vinto.
+// BONUS: scriviamo sul dom
